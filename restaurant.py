2b2c0e1a81d163b209c3b990374b62f713cbafe9
Ben Loewen <ben.loewen@computronix.com>
2023-07-27 08:55:47 -0600

    Updated art, started adding logic to restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,5 +1,39 @@
-#name = input("Welcome to Joey's good sir. My name is Wilfred and I shall be your waiter for today. May I ask what your name is?\n")
-#print("Welcome {}!".format(name))
-#print("Allow me to show you the drink menu")
-with open("desserts/dessert.txt", 'rb') as f:
-    print (f.read().decode('utf-8'))
+order = []
+name = input("\nWelcome to The Restaurant™ my name is Wilfred and I will be your waiter.\n\nWhat is your name?: ")
+print("\nWelcome {}! Let's get started!".format(name))
+
+def choices(max):
+    while True:
+        try:
+            choice = int(input("\nType the number of the item you would like to order.\nTo order nothing, type 0: "))
+            if 0 <= choice <= max:
+                return choice
+        except:
+            pass
+
+def binary(msg, opt1, opt2):
+    while True:
+        ans = input(msg)
+        if ans.upper() == opt1.upper():
+            return True
+        elif ans.upper() == opt2.upper():
+            return False
+
+def dessert():
+    ordered = []
+    desserts = [{"name": "Apple Pie", "Price": 11.50, "image": "desserts/applepie.txt"},
+                {"name": "Cherry Pie", "Price": 11.50, "image": "desserts/cherrypie.txt"},
+                {"name": "Chocolate Cake", "Price": 9.00, "image": "desserts/cake.txt"},
+                {"name": "Ice Cream Cone", "Price": 5.00, "image": "desserts/icecream.txt"},
+                {"name": "Choc. Chip Cookie", "Price": 6.00, "image": "desserts/cookie.txt"}]
+    with open("desserts/dessert.txt", 'rb') as f:
+        print("\nHere is the dessert menu {}:\n{}".format(name, f.read().decode()))
+    more = True
+    while more:
+        num = choices(5)
+        order.append(desserts[num-1])
+        ordered.append(desserts[num-1]["image"])
+        more = binary("\nDo you want to order anything else? (yes/no): ", "yes", "no")
+    return ordered
+    
+
